cec73d0a88783d7f0d0a5a4d06d3b271757f7c70
lihour <lhour8529@gmail.com>
2026-01-03 03:49:19 +0700

    change image

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,7 +9,7 @@ export default function page() {
   return (
     <div className="min-h-[400vh] bg-black overflow-hidden">
 
-      <div className=' dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative'>
+      <div className="dark:bg-black bg-white relative">
 
       <div className="max-w-7xl mx-auto p-5">
         <Navbar/>
